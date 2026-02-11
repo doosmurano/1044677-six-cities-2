aeba76cc009a868ac78a8fc629be90759c345699
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2026-02-12 00:39:31 +0300

    refactor: Рефакторинг. Функция createOffer. WIP

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,6 +1,7 @@
 import { readFileSync } from 'node:fs';
+import { Offer } from '../../types/index.js';
+import { createOffer } from '../../helpers/index.js';
 import { FileReader } from './file-reader.interface.js';
-import { Offer, User, Location, City, HousingType} from '../../types/index.js';
 
 export class TSVFileReader implements FileReader {
   private rawData = '';
@@ -8,25 +9,6 @@ export class TSVFileReader implements FileReader {
   constructor(
     private readonly filename: string
   ) {}
-
-  private parseUser(user: string): User {
-    const [name, email, avatarPath, password, isPro] = user.split(';');
-    return {
-      name,
-      email,
-      avatarPath,
-      password,
-      isPro: isPro === 'Pro'
-    };
-  }
-
-  private parseLocation(location: string) : Location {
-    const [lat, lng] = location.split(';');
-    return {
-      latitude: Number(lat),
-      longitude: Number(lng)
-    };
-  }
 
   public read(): void {
     this.rawData = readFileSync(this.filename, { encoding: 'utf-8' });
@@ -40,29 +22,6 @@ export class TSVFileReader implements FileReader {
     return this.rawData
       .split('\n')
       .filter((row) => row.trim().length > 0)
-      .map((line) => line.split('\t'))
-      .map(
-        ([
-          title, description, date, city, previewImage, images, isPremium, isFavorite, rating, type, bedrooms, maxAdults, price, goods, user, location
-        ]) => (
-          {
-            title,
-            description,
-            postDate: new Date(date),
-            city: city as City,
-            previewImage,
-            images: images.split(';'),
-            isPremium: isPremium === 'true',
-            isFavorite: isFavorite === 'true',
-            rating: Number(rating),
-            type: type as HousingType,
-            bedrooms: Number(bedrooms),
-            maxAdults: Number(maxAdults),
-            price: Number(price),
-            goods: goods.split(';'),
-            user: this.parseUser(user),
-            location: this.parseLocation(location)
-          })
-      );
+      .map((line) => createOffer(line));
   }
 }
