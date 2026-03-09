62233cbf08514dd2b580815d309426ba5b64ca4b
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2026-03-09 20:26:42 +0300

    feat: Внедрит зависимость в RestConfig. WIP

--- src/shared/libs/config/rest.config.ts
+++ src/shared/libs/config/rest.config.ts
@@ -1,13 +1,16 @@
 import { config } from 'dotenv';
 import { Logger } from '../logger/index.js';
+import { inject, injectable } from 'inversify';
 import { Config } from './config.interface.js';
+import { Component } from '../../types/index.js';
 import { configRestSchema, RestSchema } from './rest.schema.js';
 
+@injectable()
 export class RestConfig implements Config<RestSchema> {
   private readonly config: RestSchema;
 
   constructor(
-    private readonly logger: Logger
+    @inject(Component.Logger) private readonly logger: Logger
   ) {
     const parsedOutput = config();
 
