34801f7aa05c8198c1fe3174cc59c69f73546173
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2026-04-17 01:33:21 +0300

    feat: Расширит интерфейс OfferService

--- src/shared/modules/offer/offer-service.interface.ts
+++ src/shared/modules/offer/offer-service.interface.ts
@@ -2,6 +2,7 @@ import { OfferEntity } from './offer.entity.js';
 import { City } from '../../types/cities.enum.js';
 import { DocumentType } from '@typegoose/typegoose';
 import { CreateOfferDto } from './dto/create-offer.dto.js';
+import { UpdateOfferDto } from './dto/update-offer.dto.js';
 import { HousingType } from '../../types/housing-type.enum.js';
 
 export interface OfferService {
@@ -13,4 +14,11 @@ export interface OfferService {
   findByRating(rating: number): Promise<DocumentType<OfferEntity>[]>;
   findByTitle(title: string): Promise<DocumentType<OfferEntity>[]>;
   findByTitleOrCreate(offerTitle: string, dto: CreateOfferDto): Promise<DocumentType<OfferEntity>>;
+  find(): Promise<DocumentType<OfferEntity>[]>;
+  deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  updateById(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null>;
+  incCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  findNew(count: number): Promise<DocumentType<OfferEntity>[]>;
+  findDiscussed(count: number): Promise<DocumentType<OfferEntity>[]>;
+  exists(documentId: string): Promise<boolean>;
 }
