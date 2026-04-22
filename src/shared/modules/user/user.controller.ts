e9bf6d0d87381760003eb17cc629a7c2aef383e6
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2026-04-23 01:01:47 +0300

    fest: Обернёт ошибку в `next`. WIP

--- src/shared/modules/user/user.controller.ts
+++ src/shared/modules/user/user.controller.ts
@@ -1,6 +1,6 @@
-import { Response } from 'express';
 import { inject, injectable } from 'inversify';
 import { Component } from '../../types/index.js';
+import { Response, NextFunction } from 'express';
 import { Logger } from '../../libs/logger/index.js';
 import { CreateUserRequest } from './create-user-request.type.js';
 import { BaseController, HttpMethod } from '../../libs/rest/index.js';
@@ -18,8 +18,13 @@ export class UserController extends BaseController {
 
   public async create(
     _req: CreateUserRequest,
-    _res: Response
+    _res: Response,
+    next: NextFunction
   ): Promise<void> {
-    throw new Error('[UserController] Oops');
+    try {
+      throw new Error('[UserController] Oops');
+    } catch (error) {
+      return next(error);
+    }
   }
 }
