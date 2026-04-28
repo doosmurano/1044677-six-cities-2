0f2ae7640c56626daac96c3303889f2c46aaf8c3
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2026-04-29 01:06:28 +0300

    feat: Добавим обработку `HttpError` в `AppExceptionFilter`

--- src/shared/libs/rest/exception-filter/app-exception.filter.ts
+++ src/shared/libs/rest/exception-filter/app-exception.filter.ts
@@ -1,8 +1,10 @@
 import { Logger } from '../../logger/index.js';
 import { inject, injectable } from 'inversify';
+import { HttpError } from '../errors/index.js';
 import { StatusCodes } from 'http-status-codes';
 import { Component } from '../../../types/index.js';
 import { NextFunction, Request, Response } from 'express';
+import { createErrorObject } from '../../../helpers/index.js';
 import { ExceptionFilter } from './exception-filter.interface.js';
 
 @injectable()
@@ -13,10 +15,25 @@ export class AppExceptionFilter implements ExceptionFilter {
     this.logger.info('Register AppExceptionFilter');
   }
 
-  public catch(error: Error, _req: Request, res: Response, _next: NextFunction): void {
+  private handleHttpError(error: HttpError, _req: Request, res: Response, _next: NextFunction) {
+    this.logger.error(`[${error.detail}]: ${error.httpStatusCode} — ${error.message}`, error);
+    res
+      .status(error.httpStatusCode)
+      .json(createErrorObject(error.message));
+  }
+
+  private handleOtherError(error: Error, _req: Request, res: Response, _next: NextFunction) {
     this.logger.error(error.message, error);
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({error: error.message});
+      .json(createErrorObject(error.message));
+  }
+
+  public catch(error: Error | HttpError, req: Request, res: Response, next: NextFunction): void {
+    if (error instanceof HttpError) {
+      return this.handleHttpError(error, req, res, next);
+    }
+
+    this.handleOtherError(error, req, res, next);
   }
 }
