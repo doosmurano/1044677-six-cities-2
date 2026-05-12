24dcdb8a03fd0d07b32f1c03ee2d57ea742275a8
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2026-05-13 01:29:13 +0300

    feat: Добавит метод login в UserController

--- src/shared/modules/user/user.controller.ts
+++ src/shared/modules/user/user.controller.ts
@@ -7,6 +7,7 @@ import { Component } from '../../types/index.js';
 import { Logger } from '../../libs/logger/index.js';
 import { UserService } from './user-service.interface.js';
 import { Config, RestSchema } from '../../libs/config/index.js';
+import { LoginUserRequest } from './login-user-request.type.js';
 import { CreateUserRequest } from './create-user-request.type.js';
 import { BaseController, HttpError, HttpMethod } from '../../libs/rest/index.js';
 
@@ -21,6 +22,7 @@ export class UserController extends BaseController {
     this.logger.info('Register routes for UserController…');
 
     this.addRoute({ path: '/register', method: HttpMethod.Post, handler: this.create });
+    this.addRoute({ path: '/login', method: HttpMethod.Post, handler: this.login });
   }
 
   public async create(
@@ -39,5 +41,26 @@ export class UserController extends BaseController {
 
     const result = await this.userService.create(body, this.configService.get('SALT'));
     this.created(res, fillDTO(UserRdo, result));
+  }
+
+  public async login(
+    { body }: LoginUserRequest,
+    _res: Response,
+  ): Promise<void> {
+    const existsUser = await this.userService.findByEmail(body.email);
+
+    if (! existsUser) {
+      throw new HttpError(
+        StatusCodes.UNAUTHORIZED,
+        `User with email ${body.email} not found.`,
+        'UserController',
+      );
+    }
+
+    throw new HttpError(
+      StatusCodes.NOT_IMPLEMENTED,
+      'Not implemented',
+      'UserController',
+    );
   }
 }
